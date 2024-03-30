da70f6547f71d89d2337efa3a5eba2a7e814c11c
isyvdshvsy <103355375+isyvdshvsy@users.noreply.github.com>
2024-03-30 22:34:06 +0800

    Update DriverManagerFragment.kt

--- src/android/app/src/main/java/org/suyu/suyu_emu/fragments/DriverManagerFragment.kt
+++ src/android/app/src/main/java/org/suyu/suyu_emu/fragments/DriverManagerFragment.kt
@@ -35,6 +35,7 @@ import org.suyu.suyu_emu.utils.collect
 import java.io.File
 import java.io.IOException
 import android.content.Intent
+import org.suyu.suyu_emu.fragments.activitymy
 
 class DriverManagerFragment : Fragment() {
     private var _binding: FragmentDriverManagerBinding? = null
